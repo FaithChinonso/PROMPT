3747bb61e5ba1bd9b4080445381c51ba08b8e04f
Faith Umunnakwe <faithuchinonso@gmail.com>
2023-02-15 23:51:18 +0100

    night and day mode

--- src/Pages/AppPage.tsx
+++ src/Pages/AppPage.tsx
@@ -1,17 +1,32 @@
-import moment from "moment";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import LayoutApp from "../components/LayoutApp";
-import { format } from "date-fns";
+
 import LightModeIcon from "@mui/icons-material/LightMode";
 import ModeNightIcon from "@mui/icons-material/ModeNight";
+import AddTaskIcon from "@mui/icons-material/AddTask";
+import { format } from "date-fns";
+import { UiContext } from "../store/ui-context";
 
 const AppPage = () => {
+  const uiCtx = useContext(UiContext);
   const today = new Date();
   const formattedDate = format(today, "PPPP");
   return (
     <LayoutApp>
-      <div>
-        Today <span>{formattedDate}</span>
+      <div className="px-8 md:px-14 lg:px-12  py-6 lg:py-12 ">
+        <div className="flex justify-between">
+          <div className=" text-lg">
+            <span>{uiCtx.evening ? <ModeNightIcon /> : <LightModeIcon />}</span>
+            Today{" "}
+            <span className="text-sm text-darkPrimary">{formattedDate}</span>
+          </div>
+          <div className="">
+            <span>
+              <AddTaskIcon />
+            </span>
+            Add New
+          </div>
+        </div>
       </div>
     </LayoutApp>
   );
