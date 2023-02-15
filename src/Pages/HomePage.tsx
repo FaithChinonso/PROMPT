183e9ec885ff7b4da407fa079cb70b59a470fe4d
Faith Umunnakwe <faithuchinonso@gmail.com>
2023-02-15 12:21:21 +0100

    homapege

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -2,18 +2,28 @@ import React, { useState, useContext, useEffect } from "react";
 import { TodoContext } from "../store/todo-context";
 import { UiContext } from "../store/ui-context";
 import TodoType from "../Models/todo";
-import { Add } from "@mui/icons-material";
+import { Add, Man3 } from "@mui/icons-material";
 import { logout } from "../firebaseConfiq";
-
+import hero from "../assets/images/clock.jpg";
 import TodoForm from "../components/TodoForm";
 import ScheduleForm from "../components/ScheduleForm";
 import Calendar from "react-calendar";
+import { Fade, Zoom, Rotate, Reveal, Slide } from "react-reveal";
 
 import Layout from "../components/Layout";
 import { redirect } from "react-router-dom";
 import { AuthContext } from "../store/auth-context";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay } from "swiper";
+import { whatWeCanDo } from "../utils/homeArray";
 
 const HomePage: React.FC = () => {
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 600);
   const todoCxt = useContext(TodoContext);
   const uiCxt = useContext(UiContext);
   const authCxt = useContext(AuthContext);
@@ -41,13 +51,74 @@ const HomePage: React.FC = () => {
 
     setShowForms(false);
   };
-
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 600);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
   return (
     <Layout type="all">
-      <div className=" max-h-screen overflow-scroll  h-screen relative bg-red-200">
-        <div className="mt-40" onClick={() => logout()}>
-          Logout
+      <div className="flex h-[80vh] items-center justify-center">
+        <div className=" px-8 md:px-14 lg:px-24 xl:px-32 py-11 space-y-8 w-2/3 flex flex-col items-center">
+          <div className="font-thin text-xl md:text-6xl text-meduimGrey leading-10">
+            Stay ahead of the clock
+          </div>
+          <div className="text-darkGrey text-extrabold text-center text-lg leading-10">
+            Prompt offers a range of features that can help you manage your
+            tasks more efficiently, collaborate with others, and increase your
+            productivity.
+          </div>
+          <div className="bg-softPrimary text-white font-bold rounded-2xl cursor-pointer p-3 w-[200px] text-center">
+            Get Started, It's free
+          </div>
         </div>
+      </div>
+      <div className="bg-lightGrey px-8 md:px-14 lg:px-24 xl:px-32 py-11 h-[80vh]">
+        <h4 className="text-[24px] leading-[39px] text-softPrimary text-center font-semibold my-12">
+          WHY PROMPT?
+        </h4>
+        <div className="">
+          <Swiper
+            slidesPerView={isDesktop ? 2 : 2}
+            spaceBetween={30}
+            slidesPerGroup={isDesktop ? 2 : 2}
+            loop={true}
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+            }}
+            modules={[Autoplay]}
+            className="flex"
+          >
+            {whatWeCanDo.map(item => (
+              <SwiperSlide>
+                <div
+                  key={item.id}
+                  className="w-[161px] h-[101px] lg:w-[516px] lg:h-[231px] rounded-xl bg-white shadow-3xl flex items-center justify-center"
+                >
+                  <div className="w-1/2 h-full">
+                    <img
+                      src={item.img}
+                      className="object-cover w-full h-full"
+                    />
+                  </div>
+
+                  <div className="p-4 bg-accentLight h-full w-1/2">
+                    <div className="text-2xl mb-6">{item.title}</div>
+                    <div className="text-xs">{item.description}</div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+      <div></div>
+
+      {/* <div className=" max-h-screen overflow-scroll  h-screen relative bg-red-200">
+      
         <div
           className={`bg-darkPrimary -bottom-16 -right-16 flex  z-0  ${
             showForms
@@ -69,7 +140,10 @@ const HomePage: React.FC = () => {
         </div>
         {uiCxt.form === "Task" ? <TodoForm /> : ""}
         {uiCxt.form === "Schedule" ? <Calendar /> : ""}
-      </div>
+      </div> */}
+      {/* <div className="mt-40" onClick={() => logout()}>
+          Logout
+        </div> */}
     </Layout>
   );
 };
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,53 +1,21 @@
-import React, { useContext, useEffect, useState } from "react";
-import TopNav from "./TopNav";
-
-import {
-  getFirestore,
-  query,
-  getDocs,
-  collection,
-  where,
-  addDoc,
-} from "firebase/firestore";
-import { auth, db } from "../firebaseConfiq";
-import { AuthContext } from "../store/auth-context";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useNavigate } from "react-router-dom";
-
 type Props = {
   children?: React.ReactChild | React.ReactChild[];
   type: string;
 };
 const Layout: React.FC<Props> = ({ children, type }) => {
-  const authCtx = useContext(AuthContext);
-  const [user, loading, error] = useAuthState(auth);
-  // const [email, setEmail] = useState("");
-  const navigate = useNavigate();
-  const fetchDetails = async () => {
-    try {
-      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
-      const doc = await getDocs(q);
-      const data = doc.docs[0].data();
-
-      authCtx.addUser({
-        name: data.name,
-        email: data.email,
-        phone: data.phone,
-        uid: data.uid,
-      });
-    } catch (err) {
-      console.error(err);
-      alert("An error occured while fetching user data");
-    }
-  };
-  useEffect(() => {
-    if (loading) return;
-    if (!user) return navigate("/");
-    fetchDetails();
-  }, [user, loading]);
   return (
     <body className="h-screen relative bg-white py-10">
-      <TopNav type={type} />
+      <nav className="fixed top-0 left-0 flex items-center justify-between mx-auto w-full max-h-10 overflow-visible  px-8 md:px-14 lg:px-24 xl:px-32 py-11 z-10 shadow-sm shadow-lightGrey ">
+        <div className=" w-48 px-2 py-2 text-softPrimary font-extralight text-4xl italic font-['blaka'] flex-1 ">
+          PROMPT
+        </div>
+        <div className=" text-meduimGrey border border-lightGrey  rounded-2xl cursor-pointer p-3 w-[120px] mr-3 text-center hover:bg-darkPrimary hover:text-white">
+          Sign In
+        </div>
+        <div className=" text-meduimGrey rounded-2xl cursor-pointer p-3 w-[150px] text-center hover:bg-darkPrimary hover:text-white">
+          Sign Up for Free
+        </div>
+      </nav>
       <main className="mt-10">{children}</main>
     </body>
   );
